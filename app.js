273696b37bd4ed2067218e58864257149880623e
ATHARVA3005 <warokaratharva305@gmail.com>
2023-04-24 22:49:29 +0530

    atharva made some changes

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.get("/", (req, res) => {
   // Sending This is the home page! in the page
   res
     .status(200)
-    .send(`HELLO AWS  !!!`);
+    .send(`HELLO AWS xxxxxxxxxxxx !!!`);
 });
 
 // Listening to the port 
