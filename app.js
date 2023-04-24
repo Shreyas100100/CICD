1fe90826d068a1752df570a4e9763bee89ec1cc1
Shreyas Kulkarni <shreyas.22010443@viit.ac.in>
2023-04-24 22:33:21 +0530

    app

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.get("/", (req, res) => {
   // Sending This is the home page! in the page
   res
     .status(200)
-    .send(`HELLO Atharva  !!!`);
+    .send(`HELLO AWS  !!!`);
 });
 
 // Listening to the port 
