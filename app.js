d9de066212b6c98b49e1d4badf7d267f139a7b53
ak <atharvak3101@gmail.com>
2023-04-25 09:41:23 +0530

    Atharva Kulkarni made some changes

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.get("/", (req, res) => {
   // Sending This is the home page! in the page
   res
     .status(200)
-    .send(`HELLO AKSTYLES! ACKNOWLEDGE ME !!  !!!`);
+    .send(`HELLO AKSTYLES!!!`);
 });
 
 // Listening to the port 
